19c061e7a5cd0fc2d82b7a34e35c683d7e551f37
viktor <vikhulei@gmail.com>
2021-07-12 21:00:33 +0300

    counter with reducer

--- src/App.js
+++ src/App.js
@@ -1,51 +1,27 @@
 import React from "react"
 import "./App.css"
+import { useSelector, useDispatch } from "react-redux"
+import { increment, decrement } from "./action"
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fetched: [],
-      userInfo: []
-    }
-    this.getData = this.getData.bind(this)
-    this.showData = this.showData.bind(this)
-  }
+const App = () => {
+const dispatch = useDispatch()
+const counter = useSelector(state => state)
 
-async getData() {
-  const result = await fetch("https://randomuser.me/api")
-  const data = await result.json()
-  return data.results
+const increm = () => {
+  dispatch(increment)
 }
 
-async showData() {
-  const results = await this.getData()
-  this.setState(prev => ({userInfo: [...prev.userInfo, ...results]}))
+const decrem = () => {
+  dispatch(decrement)
 }
 
-async componentDidMount() {
-  const results = await this.getData()
-  this.setState({fetched: JSON.stringify(results)})
+  return <div className="App">
+    <h1>Hello</h1>
+    <h2>{counter}</h2>
+    <button onClick={increm}>+</button>
+    <button onClick={decrem}>-</button>
+  </div>
 }
-
-  render() {
-    return <div>
-    <h1>Hi</h1>
-    <p>{this.state.fetched}</p>
-    <button onClick={this.showData}>Press</button>
-    <div style={{display: "flex", flexWrap: "wrap"}}>
-    {this.state.userInfo.map((val, idx) => {
-      return <div key={idx}>
-          <p>{`${val.name.title} ${val.name.first} ${val.name.last}`}</p>
-          <img src={val.picture.large}/>
-        </div>
-    })}
-    </div>
-    </div>
-  }
-  
-}
-
 export default App
 
   
---
+++ src/reducer.js
@@ -0,0 +1,14 @@
+const counter = (state = 0, action) => {
+    switch(action.type) {
+        case "INCREMENT":
+            return state = state + 1
+            break
+        case "DECREMENT":
+            return state = state - 1
+            break
+        default:
+            return state
+    }
+}
+
+export default counter
