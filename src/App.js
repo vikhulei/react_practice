4da847c67250d3e76f72d942718bf7a6481051f3
vikhulei <65869915+vikhulei@users.noreply.github.com>
2021-11-03 21:54:22 +0200

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from "react"
-import "./App.css"
-import ToDo from "./ToDo"
-import Practice from "./Practice"
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom"
-
+import React from "react";
+import "./App.css";
+import ToDo from "./ToDo";
+import Practice from "./Practice";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
 const App = () => {
-  return <div className="App">
-    <Router>
-      <nav>
-      <Link to="/" className="navItem">Home</Link>
-      <Link to="/Practice" className="navItem">Practice</Link>
-      <Link to="/ToDo" className="navItem">ToDo</Link>
-      </nav>
-    <Switch>
-      <Route path="/Practice" component={Practice} />
-      <Route path="/ToDo" component={ToDo} />
-    </Switch>
-    </Router>
-  </div>
-}
-export default App
-
-  
-
+  return (
+    <div className="App">
+      <Router>
+        <nav>
+          <Link to="/" className="navItem">
+            Home
+          </Link>
+          <Link to="/Practice" className="navItem">
+            Practice
+          </Link>
+          <Link to="/ToDo" className="navItem">
+            ToDo
+          </Link>
+        </nav>
+        <Switch>
+          <Route path="/Practice" component={Practice} />
+          <Route path="/ToDo" component={ToDo} />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router";
+
+const Navbar = () => {
+  return <div></div>;
+};
+
+export default Navbar;
